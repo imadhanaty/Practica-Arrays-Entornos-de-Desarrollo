27ddec6f645a201762ec66150f21d3fb237a2246
imadhanaty <imad0006@alu.medac.es>
2024-05-09 10:54:43 +0200

    mitest

--- PracticaArrays/src/tests/mitest.java
+++ PracticaArrays/src/tests/mitest.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Util.MisArrays;
+
 class mitest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testMediaNotasValidas() {
+		int[] notas = { 5, 7, 8, 6, 9 };
+        assertEquals(7.0f, MisArrays.mediaNotas(notas), 0.001);
 	}
+	public void testMediaNotasInvalidas() {
+        int[] notas = { 5, 7, 11, 6, 9 };
+        MisArrays.mediaNotas(notas);
+    }
 
 }
